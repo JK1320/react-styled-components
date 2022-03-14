f1d9b69b00eae7e0fcab1dfcbbfa2ca8c43ac228
JK1320 <kaurjus1@gmail.com>
2022-03-14 22:56:22 +0000

    created Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import {StyledHeader} from "./styles/Header.styled";
 
 const Header = () => {
     return (
-        <header>
+        <StyledHeader>
             <h1>Hubble</h1>
-        </header>
+        </StyledHeader>
     )
 }
 
