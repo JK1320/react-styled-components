7a736d8b91bc4b2ca25d1857b17dd2fde7132da8
JK1320 <kaurjus1@gmail.com>
2022-03-14 22:55:38 +0000

    imported ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import Header from "./components/Header";
 import {Container} from "./components/styles/Container.styled";
+import {ThemeProvider} from "styled-components";
+import GlobalStyles from "./components/styles/Global"
+
+const theme = {
+  colors: {
+    header: "#ebfbff",
+    body: "#000",
+    footer: "#003333"
+  }
+}
 
 function App() {
   return (
-    <>
-    <Header />
-    <Container>
-     <h1>Hello World</h1>
-    </Container>
-    </>
+    <ThemeProvider theme={theme}>
+      <>
+      <GlobalStyles />
+        <Header />
+        <Container>
+          <h1>Hello World</h1>
+        </Container>
+      </>
+    </ThemeProvider>
   );
 }
 
