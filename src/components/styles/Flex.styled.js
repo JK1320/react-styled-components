032d48fcac9e56b85de0921162babfa00c4e4471
JK1320 <kaurjus1@gmail.com>
2022-03-15 11:30:49 +0000

    added media query

--- src/components/styles/Flex.styled.js
+++ src/components/styles/Flex.styled.js
@@ -1,11 +1,16 @@
 import styled from "styled-components";
 
 export const Flex = styled.div`
-display: flex;
-align-items: center;
+  display: flex;
+  align-items: center;
 
-& > div,
-& > ul {
+  & > div,
+  & > ul {
     flex: 1;
-}
+  }
+
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    flex-direction: column;
+    text-align: center;
+  }
 `
