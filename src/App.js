2bf5ee3b47940fef971cbeeeb59678fb87d75bed
JK1320 <kaurjus1@gmail.com>
2022-03-16 01:10:45 +0000

    added Card.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import Header from "./components/Header";
+import Card from "./components/Card";
 import {Container} from "./components/styles/Container.styled";
 import {ThemeProvider} from "styled-components";
-import GlobalStyles from "./components/styles/Global"
+import GlobalStyles from "./components/styles/Global";
+import content from "./content";
+import Footer from "./components/Footer";
 
 const theme = {
   colors: {
     header: "#ebfbff",
     body: "#fff",
     footer: "#003333"
-  }
+  },
+  mobile: "768px",
 }
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <>
-      <GlobalStyles />
+        <GlobalStyles />
         <Header />
         <Container>
-          <h1>Hello World</h1>
+          {content.map((item, index) => (
+            <Card key={index} item={item} />
+          ))}
         </Container>
+        <Footer />
       </>
     </ThemeProvider>
   );
